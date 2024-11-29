7dec41269668e0f7fcf1314e70489b889a075cd1
ayushpareek2003 <ayushpareek1980@gmail.com>
2024-11-30 00:59:38 +0530

    header file done

---
+++ inc/Q_lrn.hpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+
+
+class QL_RL{
+    private:
+        int Grid_Size;
+        int epochs;
+        double Alpha;
+        double gamma;
+        double epsilon;
+
+        std::vector<std::vector<int>> Data_Grid;
+        std::vector<std::vector<std::vector<double>>> qValue;
+
+        int actions[4][2]={{-1,0},{1,0},{0,1},{0,-1}};
+
+    public:
+
+    QL_RL(int Grid_size,int epochs,double Alpha,double gamma,double epsilon);
+    
+    int chooseAction(int a,int b);
+
+    void update_table(int a,int b,int action,int reward,int newX,int newY);
+
+    bool checker(int x,int y);
+
+    void trainer();
+
+    void optimumPolicy();
+
+};
+
+
